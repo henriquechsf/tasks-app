a1dc057ff83ed964fe933d0348ad6c3c37744664
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2023-03-21 15:07:45 -0300

    create fragments with view binding

---
+++ app/src/main/java/com/example/tasksapp/ui/DoingFragment.kt
@@ -0,0 +1,30 @@
+package com.example.tasksapp.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.tasksapp.R
+import com.example.tasksapp.databinding.FragmentDoingBinding
+import com.example.tasksapp.databinding.FragmentTodoBinding
+
+class DoingFragment : Fragment() {
+
+    private var _binding: FragmentDoingBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentDoingBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
