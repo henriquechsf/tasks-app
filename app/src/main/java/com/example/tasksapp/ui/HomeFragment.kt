a3baa344c77c8de641736831b97c2b3be6973f50
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2023-03-21 16:07:21 -0300

    config tabs layout

--- app/src/main/java/com/example/tasksapp/ui/HomeFragment.kt
+++ app/src/main/java/com/example/tasksapp/ui/HomeFragment.kt
@@ -5,7 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.tasksapp.R
 import com.example.tasksapp.databinding.FragmentHomeBinding
+import com.example.tasksapp.ui.adapter.ViewPagerAdapter
+import com.google.android.material.tabs.TabLayoutMediator
 
 class HomeFragment : Fragment() {
 
@@ -19,6 +22,27 @@ class HomeFragment : Fragment() {
     ): View {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initTabs()
+    }
+
+    private fun initTabs() {
+        val pageAdapter = ViewPagerAdapter(requireActivity())
+        binding.viewPager.adapter = pageAdapter
+
+        pageAdapter.addFragment(TodoFragment(), R.string.status_task_todo)
+        pageAdapter.addFragment(DoingFragment(), R.string.status_task_doing)
+        pageAdapter.addFragment(DoneFragment(), R.string.status_task_done)
+
+        binding.viewPager.offscreenPageLimit = pageAdapter.itemCount
+
+        TabLayoutMediator(binding.tabs, binding.viewPager) { tab, position ->
+            tab.text = getString(pageAdapter.getTitle(position))
+        }.attach()
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/example/tasksapp/ui/auth/LoginFragment.kt
+++ app/src/main/java/com/example/tasksapp/ui/auth/LoginFragment.kt
@@ -29,6 +29,9 @@ class LoginFragment : Fragment() {
     }
 
     private fun initListeners() {
+        binding.btnLogin.setOnClickListener {
+            findNavController().navigate(R.id.action_global_homeFragment)
+        }
         binding.btnRegister.setOnClickListener {
             findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
         }
